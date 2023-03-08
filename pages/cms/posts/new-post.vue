d8be1f5b26cc1e584850e9e9bbaf01d5183409fe
batariwah <batariwahg@gmail.com>
2023-03-08 04:06:39 +0000

    add ability to delete a tag from list

--- pages/cms/posts/new-post.vue
+++ pages/cms/posts/new-post.vue
@@ -149,12 +149,27 @@
                   :classes="{
                     input: 'checkbox',
                     wrapper: 'inline-flex gap-4 py-1',
-                    label: 'capitalize',
                   }"
                   name="tags"
                   type="checkbox"
                   :options="tags"
-                />
+                >
+                  <template #label="{ option }">
+                    <div class="badge badge-outline badge-lg gap-2 pr-0">
+                      <span class="capitalize cursor-pointer">
+                        {{ option.label }}</span
+                      >
+
+                      <label
+                        class="btn btn-square btn-xs"
+                        :class="{ 'btn-circle btn-ghost': isDark }"
+                        @click="setTagId(option.value)"
+                      >
+                        <PhX :size="18" weight="duotone" />
+                      </label>
+                    </div>
+                  </template>
+                </FormKit>
 
                 <!-- /tag -->
               </div>
@@ -165,6 +180,42 @@
 
       <!-- /tags -->
     </div>
+
+    <!-- confirm tag deletion modal -->
+    <Teleport to="body">
+      <input type="checkbox" id="delete-tag" class="modal-toggle" />
+      <div class="modal" :class="{ 'modal-open': openTagDeletionModal }">
+        <div class="modal-box relative">
+          <button
+            @click="openTagDeletionModal = false"
+            class="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            <PhX :size="20" weight="duotone" />
+          </button>
+
+          <div class="flex gap-4">
+            <PhWarningCircle :size="32" class="text-warning" weight="duotone" />
+            <div>
+              <h3 class="text-lg font-semibold">Delete tag</h3>
+              <p class="py-4">Are you sure you want to delete this tag?</p>
+            </div>
+          </div>
+          <div class="modal-action">
+            <button
+              @click="deleteTag()"
+              for="my-modal"
+              class="btn gap-2 btn-error"
+              :loading="deletingTag"
+            >
+              <PhTrashSimple v-if="!deletingTag" :size="18" weight="duotone" />
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </Teleport>
+
+    <!-- confirm tag deletion modal -->
   </section>
 </template>
 
@@ -178,6 +229,7 @@ import {
   PhPen,
   PhX,
   PhWarningCircle,
+  PhTrashSimple,
 } from "phosphor-vue";
 
 import MdEditor from "md-editor-v3";
@@ -192,8 +244,18 @@ const fields = ref({
   tags: [],
 });
 
+const { isDark } = useTheme();
+
 const fileUrl = ref("");
 const loading = ref(false);
+const tagId = ref("");
+const openTagDeletionModal = ref(false);
+const deletingTag = ref(false);
+
+const setTagId = (id) => {
+  tagId.value = id;
+  openTagDeletionModal.value = true;
+};
 
 watch(
   () => fields.value.featured_image,
@@ -258,6 +320,22 @@ const {
       d.tags.map((tag) => ({ value: tag._id, label: tag.name })),
   }
 );
+
+const deleteTag = async () => {
+  try {
+    deletingTag.value = true;
+    await $fetch(`/api/tags/${tagId.value}`, {
+      method: "DELETE",
+      headers: useRequestHeaders(["cookie"]),
+    });
+    deletingTag.value = false;
+    tagId.value = "";
+    openTagDeletionModal.value = false;
+    refresh();
+  } catch (error) {
+    deletingTag.value = false;
+  }
+};
 </script>
 
 <style scoped>
