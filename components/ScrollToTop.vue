b7926648fa129420f13a9c1bacb0be41a78e6266
batariwah <batariwahg@gmail.com>
2023-03-13 23:36:36 +0000

    add scroll-to-top button

--- components/ScrollToTop.vue
+++ components/ScrollToTop.vue
@@ -1,14 +1,40 @@
 <template>
-  <Teleport to=".wrapper">
-    <button class="btn btn-circle absolute bottom-5 right-5">
-      <PhArrowCircleUp :size="32" weight="duotone" />
-    </button>
-  </Teleport>
+  <button
+    v-show="visible"
+    @click="scrollUp"
+    class="btn btn-circle fixed bottom-8 right-8 z-50 opacity-80 hover:opacity-100"
+  >
+    <PhCaretDoubleUp :size="28" weight="duotone" />
+  </button>
 </template>
 
 <script setup>
-import { useScroll } from "@vueuse/core";
-import { PhArrowCircleUp } from "phosphor-vue";
+import { useEventListener } from "@vueuse/core";
+import { PhCaretDoubleUp } from "phosphor-vue";
 
-// const { x, y } = useScroll(, { behavior: "smooth" });
+const visible = ref(false);
+
+onMounted(() => {
+  useEventListener(document, "scroll", (e) => {
+    visible.value = window.scrollY > 150;
+  });
+});
+
+const scrollUp = () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+};
 </script>
+
+<style scoped>
+[data-theme="halloween"] .btn {
+  border-color: #3f3f46;
+}
+
+[data-theme="halloween"] .btn:hover {
+  border-color: #52525b;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="container mx-auto max-w-4xl wrapper">
+  <div class="container mx-auto max-w-4xl">
     <header class="sticky z-10 top-0 bg-base-100"><Header /></header>
     <main class="p-6 min-h-full">
       <slot />
     </main>
     <footer><Footer /></footer>
-    <!-- <ScrollToTop /> -->
+    <ScrollToTop />
   </div>
 </template>
 
